191e67f130f24bb4ef9afc6107b35e04174fec78
Gustavo Nogueira <guuhnog@gmail.com>
2024-09-18 19:22:33 -0300

    correções

--- index.ts
+++ index.ts
@@ -9,9 +9,10 @@
         this.nome = nome;
         this.email = email;
       }
+
       gerarId(): number{
         return Math.floor(Math.random() * 1000) + 1;
-    }
+      }
 }
     class Produto {
       id: number;
@@ -19,46 +20,49 @@
       preco: number;
       descricao: string;
       estoque: number;
-   
-      
+      carrinho: number;
+
       constructor(id:number, nome: string, preco: number, descricao:string) {
         this.id = id
         this.nome = nome;
         this.preco = preco;
         this.descricao = descricao
         this.estoque = this.verificarEstoque();
-    }
-      adicionarAoCarrinho():void {
-        console.log("Produto adicionado ao carrinho");
+        this.carrinho = 0;
       }
-      adicionarAoEstoque(quantidade: number):void {
-        console.log('adicionando ao estoque:', quantidade);
+      adicionarAoCarrinho(quantidade: number):number {
+        return this.carrinho = quantidade
       }
-      removerDoEstoque(quantidade: number):void {
-        console.log('removendo do estoque:', quantidade);
+      adicionarAoEstoque(quantidade: number):number {
+        return this.estoque =+ quantidade
+      }
+      removerDoEstoque(quantidade: number):number {
+        return this.estoque =- quantidade
       }
       verificarEstoque():number {
-        return Math.floor(Math.random() * 1000) + 1;
+        return this.estoque
      }
     }
     
     class CarrinhoDeCompras {
       id: number;
       total: number;
+      carrinho: number;
+
 
       constructor(id:number){
         this.id = id
         this.total = this.calcularTotal()
+        this.carrinho = 0
       }
       calcularTotal(): number { 
         return Math.floor(Math.random() * 1000) + 1;
       }
-      adicionarAoCarrinho
-      (quantidade: number): void {
-        console.log('adicionando ao estoque:', quantidade);
+      adicionarAoCarrinho(quantidade: number): number {
+        return this.carrinho =+ quantidade
       }
-      removerDoCarrinho() {
-        console.log('removendo do carrinho')
+      removerDoCarrinho(quantidade: number):number {
+        return this.carrinho =- quantidade
       }
     }
     
@@ -67,9 +71,9 @@
       data: Date;
       status: string;
       
-      constructor(id:number, data: Date, status:string,) {
+      constructor(id:number, status:string,) {
         this.id = id;
-        this.data = data;
+        this.data = new Date('2024-09-18');
         this.status = status;
         }
         
@@ -83,19 +87,43 @@
       valor: number;
       metodo: string;
       
-      constructor(id: number, valor: number, metodo: string) {
+      constructor(id: number,metodo: string) {
         this.id = id;
-        this.valor = valor;
+        this.valor = this.calcularValor();
         this.metodo = metodo;
       }
-
+      calcularValor(): number {
+        return Math.floor(Math.random() * 1000) + 1;
+      }
       pagar():void {
         console.log('Pagamento realizado com sucesso');
       }
       cancelar(): void {
-        console.log('Pagamento cancelado')
+        console.log('Pagamento cancelado');
       }
 }
     
+const usario = new Usuario('Gustavo', 'teste@gmail.com');
+console.log(usario)
 
-console.log(Usuario, Produto, CarrinhoDeCompras, Pedido, Pagamento,)
+const produto = new Produto(1,'Galaxy S24', 3000, 'Tem um grande display de 6.2 polegadas com uma resolução de 2340x1080 pixel.');
+produto.adicionarAoEstoque(15)
+produto.adicionarAoCarrinho(2)
+produto.removerDoEstoque(3)
+console.log(produto)
+
+
+const carrinho = new CarrinhoDeCompras(2)
+carrinho.adicionarAoCarrinho(3)
+carrinho.removerDoCarrinho(1)
+console.log(carrinho)
+
+const pedido = new Pedido(2, 'aguardando pagamento')
+pedido.confirmarPedido()
+console.log(pedido)
+
+const pagamento = new Pagamento(2, 'Débito')
+pagamento.pagar()
+pagamento.cancelar()
+
+
